0bc1052e1676d2ed977995e642c1aa9188ad27e0
siroyan <1517053@ed.tus.ac.jp>
2018-12-27 00:40:13 +0900

    [modify] using args

--- src/Service.java
+++ src/Service.java
@@ -1,9 +1,10 @@
 class Service{
-    Service(){
-
+    private String filePath;
+    Service(String path){
+        filePath = path;
     }
     public void read(){
         RawData rawdata = new RawData();
-        System.out.println(rawdata.getRawData("../sample.png"));
+        System.out.println(rawdata.getRawData(filePath));
     }
 }
